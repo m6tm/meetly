0aad415abc2fb891174783e65241057a27e24dbc
Madel M6TM <maboadaniel.55@gmail.com>
2025-06-20 11:52:38 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <div class="contai

--- src/components/layout/app-footer.tsx
+++ src/components/layout/app-footer.tsx
@@ -1,8 +1,8 @@
 const AppFooter = () => {
   return (
     <footer className="border-t border-border/40 py-6 md:py-8">
-      <div className="container flex flex-col items-center justify-between gap-4 md:h-16 md:flex-row">
-        <p className="text-balance text-center text-sm leading-loose text-muted-foreground md:text-left">
+      <div className="container flex flex-col items-center md:items-baseline justify-between gap-4 md:h-16 md:flex-row">
+        <p className="text-balance text-center text-sm leading-relaxed text-muted-foreground md:text-left">
           © {new Date().getFullYear()} Meetly. All rights reserved.
         </p>
         <div className="flex items-center gap-4">
