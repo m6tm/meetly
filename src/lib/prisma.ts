1fb34f5af4e0fa311920f45fb30f1a491c8989fb
m6tm <maboadaniel.55@gmail.com>
2025-08-10 09:59:34 +0100

    correction du problème de prisma 1

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,5 @@
 import { cache } from 'react';
-import { PrismaClient } from '../generated/prisma';
+import { PrismaClient } from '@prisma/client';
 
 let prismaInstance: PrismaClient | undefined;
 
