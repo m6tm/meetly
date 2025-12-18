fb7098ada8eee99818041a4696f8b354d3699f36
m6tm <maboadaniel.55@gmail.com>
2025-12-18 12:14:53 +0000

    feat: mise à jour des connexions prisma 7 avec supabase.

---
+++ prisma.config.ts
@@ -0,0 +1,14 @@
+import 'dotenv/config';
+import { defineConfig, env } from "prisma/config";
+
+/**
+ * Configuration Prisma ORM v7.
+ * Centralise les paramètres de la base de données et du schéma.
+ */
+export default defineConfig({
+  schema: 'prisma/schema.prisma',
+  datasource: {
+    url: env('DATABASE_URL'),
+    shadowDatabaseUrl: env('DIRECT_URL'),
+  },
+});
